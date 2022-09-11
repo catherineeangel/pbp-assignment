e16a1e09bdb0a0a01e8e2636174b84174ead5732
Catherine Angel <catheriner40540@gmail.com>
2022-09-11 23:33:34 +0700

    add views and edit template

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+data_barang_wishlist = CatalogItem.objects.all()
+context = {
+    'list_katalog': data_barang_wishlist,
+    'name': 'Catherine',
+    'student_id': '2106705392'
+}
+
+def show_catalog(request):
+    return render(request, "katalog.html", context)
