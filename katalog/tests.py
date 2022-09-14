f91d7ed628650448cb0afdc353076d1787da586b
Catherine Angel <catheriner40540@gmail.com>
2022-09-14 09:18:59 +0700

    add tests

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from katalog.models import CatalogItem
 # Create your tests here.
+class KatalogTestCase(TestCase):
+    def setUp(self):
+        CatalogItem.objects.create(item_name="Test Item", item_price=2000000, item_stock=999, description="Test description", rating=6, item_url="https://www.tokopedia.com/")
+
+    def test_this(self):
+        test = CatalogItem.objects.get(item_name="Test Item")
+        self.assertTrue(test.item_price, 2000000)
